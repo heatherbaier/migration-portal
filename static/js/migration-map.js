b3d8de7eeb33b94153185e8669cbbfbff09aaeeb
heatherbaier <hmbaier@email.wm.edu>
2021-05-10 12:12:28 -0400

    #map legend #26

--- static/js/migration-map.js
+++ static/js/migration-map.js
@@ -7,6 +7,8 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{
 }).addTo(mymap);
 
 
+
+
 // Create the drawnPolys group
 window.drawnPolys = new L.featureGroup().addTo(mymap);
 
@@ -136,6 +138,24 @@ function polygon_style(feature) {
     fillOpacity: 0.5
   };
 }
+
+var legend = L.control({position: 'bottomleft'});
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? ' &ndash; ' + grades[i + 1] + ' migrants<br>' : '+ migrants');
+    }
+
+    return div;
+};
+legend.addTo(mymap);
 
 
 // Removes highlight from polygons when cursor is not over them
